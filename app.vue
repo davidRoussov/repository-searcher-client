af8fb8b0e9417839619acae9602f9035114bdf36
David Roussov <david.roussov@gmail.com>
2023-08-04 10:58:11 +1000

    add bootstrap

--- app.vue
+++ app.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <RepositoriesDataTable/>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-12">
+        <div class="mb-3">
+          <label for="nameInput" class="form-label">Name</label>
+          <input
+            type="text"
+            class="form-control form-control-lg"
+            id="nameInput"
+            placeholder="Repository name"
+            v-model="name"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <RepositoriesDataTable/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default defineComponent({
+  data: function() {
+    return {
+      name: '',
+    };
+  },
 });
 </script>
