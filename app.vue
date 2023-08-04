0397680f4dbdf75e8ea2a592237319d0f3ad4a98
David Roussov <david.roussov@gmail.com>
2023-08-04 10:02:18 +1000

    initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  created: function() {
+    console.log('hello, world!');
+  },
+});
+</script>
